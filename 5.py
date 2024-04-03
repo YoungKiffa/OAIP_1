fc29df2700e391e0f3a6e1039ef61d20a23cfcd3
YoungKiffa <144680295+YoungKiffa@users.noreply.github.com>
2024-04-03 12:29:08 +0900

    5.py

--- 5.py
+++ 5.py
@@ -1,20 +1,20 @@
 def main():
-a = open("lines.txt", encoding="utf-8")
-b = 0
-for line in a:
-    b += len(line)
-    c = b * 8
-    d = b
-    e = b / 1000
-    f = b / 106
-    g = b / 109
-    h = b / 10**12
-print(c, "бит")
-print(d, "байт")
-print(e, "мегабайт")
-print(f, "мегабайт")
-print(g, "гигабайт")
-print(h, "терабайт")
+    with open("words.txt", encoding="utf-8") as a:
+        b = 0
+        for line in a:
+            b += len(line)
+        c = b * 8
+        d = b
+        e = b / 1000
+        f = b / 106
+        g = b / 109
+        h = b / 10 ** 12
+    print(c, "бит")
+    print(d, "байт")
+    print(e, "мегабайт")
+    print(f, "мегабайт")
+    print(g, "гигабайт")
+    print(h, "терабайт")
 
 
 if __name__ == "__main__":
